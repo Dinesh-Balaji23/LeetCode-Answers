160a51cd3618320d568b5b14d371a408bcd552b5
Dineshbalaji Anbarasan <dineshbalaji.a.2005@gmail.com>
2025-12-20 09:03:44 +0530

    Time: 86 ms (8.01%) | Memory: 46.9 MB (31.07%) - LeetSync

---
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -0,0 +1,27 @@
+class Solution {
+    public ListNode merge(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        ListNode tail = head;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                tail.next = l1;
+                l1 = l1.next;
+            } else {
+                tail.next = l2;
+                l2 = l2.next;
+            }
+            tail = tail.next;
+        }
+        tail.next = (l1 != null) ? l1 : l2;
+        return head.next;
+    }
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) return null;
+        ListNode head = lists[0];
+        for (int i = 1; i < lists.length; i++) {
+            head = merge(head, lists[i]);
+        }
+        return head;
+    }
+}
