f77a7faa9cd28a66b5dc9575d00be658ad293c66
Dineshbalaji Anbarasan <dineshbalaji.a.2005@gmail.com>
2025-12-31 14:18:41 +0530

    Time: 2 ms (73.85%) | Memory: 55.8 MB (15.01%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        int candidate = nums[0];
+        for(int i = 0; i < nums.length; i++) {
+            if(candidate == nums[i]) count++;
+            else count--;
+            if(count == 0) candidate = nums[i + 1];
+        }
+        return candidate;
+    }
+}
