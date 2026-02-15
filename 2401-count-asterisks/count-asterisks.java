cce34dcf389b04aa9a7bb475048b85ec80f9ad16
Dineshbalaji Anbarasan <dineshbalaji.a.2005@gmail.com>
2026-02-15 21:29:22 +0530

    Time: 1 ms (99.18%) | Memory: 43.3 MB (6.19%) - LeetSync

---
+++ 2401-count-asterisks/count-asterisks.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int countAsterisks(String s) {
+        int count = 0;
+        boolean in = false;
+        for (char ch : s.toCharArray()) {
+            if (ch == '|') in = !in;
+            else if (ch == '*' && !in) count++;
+        }
+        return count;
+    }
+}
